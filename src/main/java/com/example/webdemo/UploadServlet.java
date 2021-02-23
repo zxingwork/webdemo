fa6ea87ffb51f683500ebbba174329b5f2ff65f0
zxingwork <zxingwork@163.com>
2021-02-24 01:56:55 +0800

    add

--- src/main/java/com/example/webdemo/UploadServlet.java
+++ src/main/java/com/example/webdemo/UploadServlet.java
@@ -13,9 +13,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Enumeration;
 import java.util.List;
 
@@ -23,23 +21,24 @@ import java.util.List;
 public class UploadServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Enumeration<String> names = req.getAttributeNames();
-        while (names.hasMoreElements()){
-            System.out.println("attribute: "+names.nextElement());
-        }
-
-        ServletInputStream inputStream = req.getInputStream();
-        byte[] bytes = new byte[1024];
-        int len = 0;
-        while ((len=inputStream.read(bytes))>0){
-            System.out.println("in in");
-            System.out.println(new String(bytes, 0, len));
-        }
+//        Enumeration<String> names = req.getAttributeNames();
+//        while (names.hasMoreElements()){
+//            System.out.println("attribute: "+names.nextElement());
+//        }
+//
+//        ServletInputStream inputStream = req.getInputStream();
+//        byte[] bytes = new byte[1024];
+//        int len = 0;
+//        while ((len=inputStream.read(bytes))>0){
+//            System.out.println("in in");
+//            System.out.println(new String(bytes, 0, len));
+//        }
         try {
             DiskFileItemFactory factory = new DiskFileItemFactory();
             ServletFileUpload upload = new ServletFileUpload(factory);
 
             if (!ServletFileUpload.isMultipartContent(req)){
+                System.out.println("return");
                 return;
             }
             List<FileItem> list = upload.parseRequest(req);
@@ -51,22 +50,37 @@ public class UploadServlet extends HttpServlet {
                     System.out.println(name+"="+value);
                 }else {
                     String filename = item.getFieldName();
+
+                    System.out.println(filename);
                     filename = filename.substring(filename.lastIndexOf("\\")+1);
+                    System.out.println("filename:"+filename);
                     InputStream in = item.getInputStream();
-                    int le = 0;
+                    int len = 0;
                     byte[] buffer = new byte[1024];
                     String savepath = this.getServletContext().getRealPath("/upload");
-                    FileOutputStream out = new FileOutputStream(savepath+"\\"+filename);
-                    while ((le=in.read(buffer))>0){
-                        out.write(buffer,0,le);
+                    System.out.println("path:"+savepath+"/"+filename);
+                    File folder = new File(savepath);
+                    if (!folder.exists()||!folder.isDirectory()){
+                        folder.mkdirs();
+                        System.out.println("创建文件夹");
+                    }else System.out.println("文件夹存在");
+                    FileOutputStream out = new FileOutputStream(savepath+"/"+filename);
+                    while ((len=in.read(buffer))>0){
+
+                        out.write(buffer,0,len);
                     }
                     in.close();
                     out.close();
+                    String rs = "上传成功！";
+                    resp.setContentType("text/html;charset=UTF-8");
+                    PrintWriter printWriter = resp.getWriter();
+                    printWriter.write(rs);
                 }
             }
 
         } catch (FileUploadException e) {
             e.printStackTrace();
         }
+
     }
 }
